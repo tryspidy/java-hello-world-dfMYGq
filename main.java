227934abd6911f25379da5f3375cae881c9c843d
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 18:45:28 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,8 @@
+public class PrintHelloWorld 
+{
+  public static void main(String[] args) 
+  {
+    //This is the print line. Anything in the Quotation Maks will be printed
+  	System.out.println("Hello, World!"); 
+  }
+}
